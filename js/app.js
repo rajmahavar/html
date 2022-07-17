8d81fbcf585b576db4108d2e3004a4e4d630a640
Lekhraj <rajmahavar@gmail.com>
2022-07-17 23:00:36 +0530

    js update

--- js/app.js
+++ js/app.js
@@ -2,3 +2,22 @@
 const menuToggle = () => {
     $("#mobileMenu").toggleClass("opened");
 }
+
+
+// const playBtn = () => {
+//     let playBtn = document.getElementById("playButton");
+//     playBtn.play();
+//     alert('sfsfsfsf')
+// }
+
+const playBtn = () => {
+    let videoPlayer = document.getElementById("videoPlayer");
+    videoPlayer.play();
+    $(".playButton").fadeOut(500);
+}
+
+// var player = document.getElementById("videoPlayer");
+// player.addEventListener('click', function(){
+//     videoPlayer.pause();
+//     $(".playButton").fadeIn(500);
+// })
